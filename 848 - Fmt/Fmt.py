c346e75ceecef8e99ae2b45db4b0536d3fcc4d08
Jose Sebastián Canós <ep3p@users.noreply.github.com>
2019-01-08 11:41:50 +0100

    Update Fmt.py

--- 848 - Fmt/Fmt.py
+++ 848 - Fmt/Fmt.py
@@ -1,39 +1,35 @@
 import sys
 
-def prent(cont):
-	if len(cont) != 0:
-		print((" ".join(cont)).rstrip(), sep="", end="\n")
-
+def fmt_print(buf):
+    if len(buf):
+        print((" ".join(buf)).rstrip(), sep="", end="\n")
 
 lines = iter(sys.stdin.read().splitlines())
 
+buf = []
 n = -1
-cont = []
 
 for line in lines:
-	if line == "":
-		prent(cont)
-		cont = []
-		n = -1
-		print("\n", sep="", end="")
-		
-	else:
-		if line[0] == " ":
-			prent(cont)
-			cont = []
-			n = -1
-		sep = " "
-		rest = line
-		while sep != "":
-			bef,sep,rest = rest.partition(" ")
-			if n == 72 and bef == "":
-				pass
-			
-			elif n + 1 + len(bef) > 72:
-				prent(cont)
-				cont = [bef]
-				n = len(bef)
-			else:
-				cont.append(bef)
-				n += 1 + len(bef)
-prent(cont)
+    if not line:
+        fmt_print(buf)
+        print("\n", sep="", end="")
+        buf = []
+        n = -1
+    else:
+        if line[0] == ' ':
+            fmt_print(buf)
+            buf = []
+            n = -1
+        sep = ' '
+        rest = line
+        while sep:
+            bef, sep, rest = rest.partition(' ')
+            if n != 72 or bef:
+                if n + 1 + len(bef) > 72:
+                    fmt_print(buf)
+                    buf = [bef]
+                    n = len(bef)
+                else:
+                    buf.append(bef)
+                    n += 1 + len(bef)
+fmt_print(buf)
