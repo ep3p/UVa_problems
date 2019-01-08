c506b8f285b623c740c722f125805444a03aaf83
Jose Sebastián Canós <ep3p@users.noreply.github.com>
2019-01-08 15:50:53 +0100

    Update Crypt Kicker II.py

--- 850 - Crypt Kicker II/Crypt Kicker II.py
+++ 850 - Crypt Kicker II/Crypt Kicker II.py
@@ -1,57 +1,29 @@
 import sys
 
-lines = iter(sys.stdin.read().splitlines())
+pangram = 'the quick brown fox jumps over the lazy dog'
 
-T = int(next(lines))
+lines = iter(sys.stdin.read().splitlines())
+N = int(next(lines))
 next(lines)
 
-out = []
+result = []
 
-for i in range(T):
-	
-	sent = []
-	j = 0
-	
-	for line in lines:
-		num = line.split()
-		if len(num) == 0:
-			if line == "":
-				break
-		else:
-			if j < 100:
-				sent.append(line)
-			j += 1
-	
-	dict = None
-	
-	for s in sent:
-		words = s.split()
-		if 	len(words) == 9 and \
-			len(words[0]) == 3 and \
-			len(words[1]) == 5 and \
-			len(words[2]) == 5 and \
-			len(words[3]) == 3 and \
-			len(words[4]) == 5 and \
-			len(words[5]) == 4 and \
-			len(words[6]) == 3 and \
-			len(words[7]) == 4 and \
-			len(words[8]) == 3 and \
-			words[0] == words[6] and \
-			words[0][2] == words[5][2] and \
-			words[1][1] == words[4][1] and \
-			words[2][1] == words[5][3] and \
-			words[2][2] == words[3][1] and \
-			words[2][2] == words[5][0] and \
-			words[2][2] == words[8][1]:
-			dict = "".maketrans(" ".join(words),"the quick brown fox jumps over the lazy dog")
-			if " ".join(words).translate(dict) != "the quick brown fox jumps over the lazy dog":
-				dict = None
-			else:
-				break
+for i in range(N):
+    to_translate = []    
+    for line in lines:
+        if not line:
+            break
+        else:
+            to_translate.append(line)
+        
+    for item in to_translate:
+        phrase = ' '.join(item.split())
+        if len(phrase) == len(pangram):
+            table = str.maketrans(phrase, pangram)
+            if phrase.translate(table) == pangram:
+                result.append(''.join(map(lambda x : x.translate(table) + '\n', to_translate)))
+                break
+    else:
+        result.append('No solution.\n')
 
-	if dict is not None:
-		out.append("\n".join(map(lambda x : x.translate(dict),sent))+"\n")
-	else:
-		out.append("No solution.\n")
-
-print("\n".join(out),sep="",end="")
+print('\n'.join(result), sep='', end='')
