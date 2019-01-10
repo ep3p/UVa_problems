e5f78dadceb0695c277d85484f976c6c0eb4b3ee
Jose Sebastián Canós <ep3p@users.noreply.github.com>
2019-01-10 14:20:40 +0100

    Update Hartals.py

--- 10050 - Hartals/Hartals.py
+++ 10050 - Hartals/Hartals.py
@@ -1,15 +1,17 @@
-N = int(input())
-for _ in range(N):  
-    duration = int(input()) 
+lines = iter(sys.stdin.read().splitlines())
+
+N = int(next(lines))
+for _ in range(N):
+    duration = int(next(lines))
     week = [False]*5 + [True]*2
     n_weeks, r_days = divmod(duration, 7)
     days = week*n_weeks + week[:r_days]
     weekends = 2*n_weeks + int(r_days > 5)
     
     parties = set()
-    n_parties = int(input())
+    n_parties = int(next(lines))
     for _ in range(n_parties):
-        parties.add(int(input()))
+        parties.add(int(next(lines)))
     
     for period in parties:
         for day in range(period-1, duration, period):
